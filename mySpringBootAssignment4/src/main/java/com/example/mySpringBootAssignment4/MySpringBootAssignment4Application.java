1a3aa958f2d1d410ba632d799031f3a82fdb6820
mukesh-bachu <105143118+mukesh-bachu@users.noreply.github.com>
2023-06-26 18:35:18 -0500

    Add files via upload

---
+++ mySpringBootAssignment4/src/main/java/com/example/mySpringBootAssignment4/MySpringBootAssignment4Application.java
@@ -0,0 +1,13 @@
+package com.example.mySpringBootAssignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAssignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAssignment4Application.class, args);
+		System.out.println("Hello World!");
+	}
+}
---
+++ mySpringBootAssignment4/src/test/java/com/example/mySpringBootAssignment4/MySpringBootAssignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mySpringBootAssignment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootAssignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
